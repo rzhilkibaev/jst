837b05419b318d0663b8ac0b9161c4acec9f11fc
Renat <rzhilkibaev@gmail.com>
2015-02-04 17:04:06 -0800

    Fix src build

--- jst/resources/src.py
+++ jst/resources/src.py
@@ -15,7 +15,7 @@ def execute(action, args, ctx):
     if action == 'init':
         _init(buildomatic_dir, ctx)
     elif action == 'build':
-        _build(args, buildomatic_dir)
+        _build(buildomatic_dir, args)
     else:
         _execute_shell_action_on_working_copy(action, ctx['src']['url_ce'], ctx['src']['working_copy_ce'])
         _execute_shell_action_on_working_copy(action, ctx['src']['url_pro'], ctx['src']['working_copy_pro'])
