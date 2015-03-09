115644b8cdd9e5a252a0a6213154d18bb9ccc075
Renat <rzhilkibaev@gmail.com>
2015-03-09 15:32:59 -0700

    Download tomcat distribution and use it as a source for workspaces

--- jst/resources/context.py
+++ jst/resources/context.py
@@ -10,13 +10,28 @@ import shutil
 
 
 _app_name = "jst"
+_app_home = os.path.dirname(__file__).replace("/jst/resources", "")
+
+def _ensure_core_properties(ctx):
+    data_dir = expanduser("~") + "/." + _app_name
+    _ensure_property(ctx, "core", "data_dir", data_dir)
+
+    _ensure_property(ctx, "core", "download_cache", data_dir + "/download_cache")
+    if (not os.path.exists(ctx["core"]["download_cache"])):
+        os.makedirs(ctx["core"]["download_cache"])
+
+    _ensure_property(ctx, "core", "templates", data_dir + "/templates")
+    try:
+        shutil.copytree(_app_home + "/templates", ctx["core"]["templates"])
+    except FileExistsError:
+        pass
 
 
 def load(args):
 
     ctx = configparser.ConfigParser()
 
-    _ensure_property(ctx, 'core', 'data_dir', expanduser("~") + "/." + _app_name)
+    _ensure_core_properties(ctx)
 
     _load_user_properties(ctx)
     _load_workspace_properties(ctx)
@@ -28,7 +43,7 @@ def load(args):
 
 
 def _load_user_properties(ctx):
-    user_properties_file = ctx['core']['data_dir'] + "/" + _app_name + ".properties"
+    user_properties_file = ctx["core"]["data_dir"] + "/" + _app_name + ".properties"
     default_user_properties_file = os.path.dirname(__file__) + "/" + "default.user.properties"
 
     _ensure_file_present(user_properties_file, default_user_properties_file)
@@ -83,8 +98,8 @@ def _ensure_all_properties(ctx):
     _ensure_property(ctx, "src", "working_copy_pro", cwd + "/pro")
 
     # tc
+    _ensure_property(ctx, "tc", "distribution_url", "http://www.gtlib.gatech.edu/pub/apache/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz")
     _ensure_property(ctx, "tc", "home", cwd + "/tomcat")
-    _ensure_property(ctx, "tc", "distribution", ctx['core']['data_dir'] + "/lib/tomcat")
     _ensure_property(ctx, "tc", "java_opts", "-Dport.http=8080 -Dport.ajp=8009 -Dport.shutdown=8005")
     _ensure_property(ctx, "tc", "catalina_opts", "-agentlib:jdwp=transport=dt_socket,address=1044,server=y,suspend=n -Djavax.xml.soap.SOAPFactory=org.apache.axis.soap.SOAPFactoryImpl -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl -Djavax.xml.soap.SOAPConnectionFactory=org.apache.axis.soap.SOAPConnectionFactoryImpl -Djavax.xml.soap.MessageFactory=org.apache.axis.soap.MessageFactoryImpl -Djava.net.preferIPv4Stack=true -Xms1024m -Xmx2048m -XX:PermSize=32m -XX:MaxPermSize=512m -Xss2m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled")
 
@@ -116,7 +131,6 @@ def _show_context(ctx):
     print("src.working_copy_pro = " + ctx["src"]["working_copy_pro"])
 
     print("tc.home = " + ctx["tc"]["home"])
-    print("tc.distribution = " + ctx["tc"]["distribution"])
     print("tc.catalina_opts = " + ctx["tc"]["catalina_opts"])
     print("tc.java_opts = " + ctx["tc"]["java_opts"])
 
--- jst/resources/tc.py
+++ jst/resources/tc.py
@@ -1,34 +1,37 @@
-'''
+"""
 Created on Jan 7, 2015
 
 @author: rz
-'''
+"""
+from distutils import dir_util
 import glob
 import os
+from os.path import basename
 import psutil
 import re
 import shutil
 import subprocess
 from time import sleep
+import urllib.request
 
 
 def execute(action, args, ctx):
     """ executes an action against a tomcat instance """
 
-    if action == 'init':
+    if action == "init":
         _init(ctx)
-    elif action == 'status':
+    elif action == "status":
         _show_status(ctx)
-    elif action == 'go':
+    elif action == "go":
         _go(ctx)
-    elif action == 'deploy':
+    elif action == "deploy":
         _deploy(args, ctx)
-    elif action == 'restart':
-        _execute_catalina_action('stop', ctx)
+    elif action == "restart":
+        _execute_catalina_action("stop", ctx)
         sleep(6)
-        _execute_catalina_action('start', ctx)
+        _execute_catalina_action("start", ctx)
         _show_status(ctx)
-    elif action == 'start':
+    elif action == "start":
         _execute_catalina_action(action, ctx)
         _show_status(ctx)
     else:
@@ -37,30 +40,37 @@ def execute(action, args, ctx):
 
 
 def _init(ctx):
-    try:
-        shutil.copytree(ctx['tc']['distribution'], ctx['tc']['home'])
-    except FileExistsError:
-        print('already initialized')
+    tomcat_distribution_file = ctx["core"]["download_cache"] + "/" + basename(ctx["tc"]["distribution_url"])
+
+    if (not os.path.isfile(tomcat_distribution_file)):
+        tomcat_distribution_file = urllib.request.urlretrieve(ctx["tc"]["distribution_url"], tomcat_distribution_file)[0]
+
+    if (not os.path.exists(ctx["tc"]["home"])):
+        os.makedirs(ctx["tc"]["home"])
+
+    subprocess.call(["tar", "-xf", tomcat_distribution_file, "-C", ctx["tc"]["home"], "--strip-components=1"])
+
+    dir_util.copy_tree(ctx["core"]["templates"] + "/tomcat", ctx["tc"]["home"])
 
 
 
 def _show_status(ctx):
-    catalina_main_class_arg = 'org.apache.catalina.startup.Bootstrap'
-    catalina_home_arg = '-Dcatalina.home=' + ctx['tc']['home']
+    catalina_main_class_arg = "org.apache.catalina.startup.Bootstrap"
+    catalina_home_arg = "-Dcatalina.home=" + ctx["tc"]["home"]
 
     for proc in psutil.process_iter():
-        if proc.name == 'java' \
+        if proc.name == "java" \
                 and catalina_main_class_arg in proc.cmdline \
                 and catalina_home_arg in proc.cmdline:
             http_port = _get_http_port(proc.cmdline)
             debug_port = _get_debug_port(proc.cmdline)
-            print('pid=' + str(proc.pid) + ', port.http=' + str(http_port) + ', port.jdwp=' + str(debug_port))
+            print("pid=" + str(proc.pid) + ", port.http=" + str(http_port) + ", port.jdwp=" + str(debug_port))
 
 
 
 def _get_http_port(catalina_args):
     for catalina_arg in catalina_args:
-        match = re.search('-Dport\.http=(\d*)', catalina_arg)
+        match = re.search("-Dport\.http=(\d*)", catalina_arg)
         if match:
             return match.group(1)
 
@@ -68,17 +78,17 @@ def _get_http_port(catalina_args):
 
 def _get_debug_port(catalina_args):
     for catalina_arg in catalina_args:
-        match = re.search('-agentlib:.*,address=(\d*)', catalina_arg)
+        match = re.search("-agentlib:.*,address=(\d*)", catalina_arg)
         if match:
             return match.group(1)
 
 
 
 def _go(ctx):
-    match = re.search('-Dport\.http=(\d*)', ctx['tc']['java_opts'])
+    match = re.search("-Dport\.http=(\d*)", ctx["tc"]["java_opts"])
     if match:
         http_port = match.group(1)
-        subprocess.call(['xdg-open', 'http://localhost:' + http_port + '/jasperserver-pro'],
+        subprocess.call(["xdg-open", "http://localhost:" + http_port + "/jasperserver-pro"],
                         stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
 
@@ -86,26 +96,27 @@ def _go(ctx):
 def _deploy(args, ctx):
 
     if len(args) == 0:
-        build_xml = ctx['src']['working_copy_ce'] + '/buildomatic/build.xml'
-        subprocess.call(['ant', '-buildfile', build_xml, 'deploy-webapp-pro'])
+        build_xml = ctx["src"]["working_copy_ce"] + "/buildomatic/build.xml"
+        subprocess.call(["ant", "-buildfile", build_xml, "deploy-webapp-pro"])
     else:
         edition = args[0]
-        source = ctx['src']['working_copy_' + edition] + '/' + args[1] + '/target/*.jar'
+        source = ctx["src"]["working_copy_" + edition] + "/" + args[1] + "/target/*.jar"
         files = glob.glob(source)
-        destination = ctx['tc']['home'] + '/webapps/jasperserver-pro/WEB-INF/lib/'
+        destination = ctx["tc"]["home"] + "/webapps/jasperserver-pro/WEB-INF/lib/"
         if (len(files)):
             shutil.copy(files[0], destination)
             print(files[0])
         else:
-            print('nothing to copy at: ' + source)
+            print("nothing to copy at: " + source)
 
 
 
 def _execute_catalina_action(action, ctx):
     """ executes catalina.sh script passing action as an argument """
 
-    os.environ['CATALINA_HOME'] = ctx['tc']['home']
-    os.environ['CATALINA_OPTS'] = ctx['tc']['catalina_opts']
-    os.environ['JAVA_OPTS'] = ctx['tc']['java_opts']
-    catalina = ctx['tc']['home'] + '/bin/catalina.sh'
+    os.environ["CATALINA_HOME"] = ctx["tc"]["home"]
+    os.environ["CATALINA_OPTS"] = ctx["tc"]["catalina_opts"]
+    os.environ["JAVA_OPTS"] = ctx["tc"]["java_opts"]
+    catalina = ctx["tc"]["home"] + "/bin/catalina.sh"
     subprocess.call([catalina, action])
+
