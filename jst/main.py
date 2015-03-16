18c5b66b1d5773ce71328a4ed9db29dee5e3d488
Renat <rzhilkibaev@gmail.com>
2015-03-16 16:33:13 -0700

    Simplify command line arguments

--- jst/main.py
+++ jst/main.py
@@ -1,5 +1,5 @@
 """"
-Usage: jst [-h|--help] <resource> <action> [<args>...]
+Usage: jst [-h|--help] <resource> [<action>] [<args>...]
 
 This tool manages jrs instances.
 
@@ -40,38 +40,102 @@ jst src build|rebuild     Build both ce and pro sources (ant build-src-all)
 jst src build|rebuild pro|ce <directory>
                           Build a single ce/pro directory
                           optional argument: --skip-tests=[true|false] to skip tests
+                          
+                          
+init
+status                   tomcat, --verbose gives context info
+
+#todo: svn <any_svn_command>
+# svn shortcuts
+checkout
+status
+diff
+revert
+
+start
+stop
+restart
+
+build --skip-tests
+deploy
+go
 """
 from docopt import docopt
 
 from resources import context
+from resources import src
+from resources import tc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    args = docopt(__doc__, version = '0.1', options_first = True)
+    args = docopt(__doc__, version = "0.1", options_first = True)
 
-    resource = args['<resource>']
-    action = args['<action>']
-    action_args = args['<args>']
+    resource = args["<resource>"]
+    action = args["<action>"]
+    action_args = args["<args>"]
 
     ctx = context.load(args)
 
-    if (resource == 'tc'):
-
-        from resources import tc
+    if (resource == "tc"):
 
         tc.execute(action, action_args, ctx)
 
-    elif (resource == 'src'):
-
-        from resources import src
+    elif (resource == "src"):
 
         src.execute(action, action_args, ctx)
 
-    elif (resource == 'ctx'):
+    elif (resource == "ctx"):
 
         context.execute(action, action_args, ctx)
 
+    elif (resource == "init"):
+
+        tc.execute("init", action_args, ctx)
+        src.execute("init", action_args, ctx)
+
+    elif (resource == "status"):
+
+        tc.execute("status", action_args, ctx)
+        context.execute("show", action_args, ctx)
+        src.execute("status", action_args, ctx)
+
+    elif (resource == "checkout"):
+
+        src.execute("checkout", action_args, ctx)
+
+    elif (resource == "diff"):
+
+        src.execute("diff", action_args, ctx)
+
+    elif (resource == "revert"):
+
+        src.execute("revert", action_args, ctx)
+
+    elif (resource == "start"):
+
+        tc.execute("start", action_args, ctx)
+
+    elif (resource == "stop"):
+
+        tc.execute("stop", action_args, ctx)
+
+    elif (resource == "restart"):
+
+        tc.execute("restart", action_args, ctx)
+
+    elif (resource == "build"):
+
+        src.execute("build", action_args, ctx)
+
+    elif (resource == "deploy"):
+
+        tc.execute("deploy", action_args, ctx)
+
+    elif (resource == "go"):
+
+        tc.execute("go", action_args, ctx)
+
     else:
-        print('Unknown resource: ' + resource)
+        print("Unknown resource: " + resource)
 
